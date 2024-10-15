7eadc6a8fad9c60635a30d2b36596b7f8bec8bf7
ChedlyRebai <chedly.rebai123@gmail.com>
2024-10-15 19:14:32 +0200

    Refactor Author class: Remove unnecessary whitespace

--- src/Controller/BookController.php
+++ src/Controller/BookController.php
@@ -57,7 +57,17 @@ class BookController extends AbstractController
         ]);
     }
 
+    #[Route('/', name: 'author_index', methods: ['GET'])]
+    public function index(AuthorRepository authorRepository): Response {
+        console.log('Fetching all authors');
+        return this->render('author/index.html.twig', [
+            'authors' => authorRepository->findAll(),
+        ]);
+    }
 
+
+
+    
     #[Route('/{id}/edit',name:'book_edit',methods:['GET','POST'])]
     public function edit(Request $request,Book $book,EntityManagerInterface $entitymanager):Response {
         if($request->isMethod('POST')){
--- src/Entity/Author.php
+++ src/Entity/Author.php
@@ -90,5 +90,4 @@ class Author
 }
 
 
-  
 }
