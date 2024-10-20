e5d073d4879051d3005b4b9bc0d15fe5846cf1dc
ChedlyRebai <chedly.rebai123@gmail.com>
2024-10-20 12:32:53 +0200

    Refactor book controller: Update index, new, edit, and delete actions

--- src/Controller/BookController.php
+++ src/Controller/BookController.php
@@ -19,92 +19,75 @@ use Symfony\Component\Routing\Annotation\Route;
 #[Route('/book')]
 class BookController extends AbstractController
 {
-    #[Route('/',name:'book_index',methods:['GET'])]
+    #[Route('/', name: 'book_index', methods: ['GET'])]
     public function index(BookRepository $bookrepo): Response
     {
-        return $this->render('book/index.html.twig',[
-            'books'=>$bookrepo->findAll(),
+        return $this->render('book/index.html.twig', [
+            'books' => $bookrepo->findAll(),
         ]);
     }
 
-    #[Route('/new',name:'book_new',methods:['GET','POST'])]
-    public function new(AuthorRepository $authorepo, Request $request,EntityManagerInterface $entitymanager):Response {
-        if($request->isMethod('POST')){
-            $book = new Book();
-            
-            $book->setTitle($request->request->get('title'));
-            $publishedDate = $request->request->get('published_date');
-            if ($publishedDate) {
-                $book->setPublicationDate(new \DateTime($publishedDate));
-            } else {
-                throw new \Exception('Published date is required');
-            }
+    #[Route('/new', name: 'book_new', methods: ['GET', 'POST'])]
+    public function new(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $book = new Book();
+        $form = $this->createForm(BookType::class, $book);
+        $form->handleRequest($request);
 
-            //$book->setPublicationDate($request->request->get('published_date'));
-            $book->setEnabled($request->request->get('enabled'));
-            $author = $entitymanager->getRepository(Author::class)->find($request->request->get('author'));
+        if ($form->isSubmitted() && $form->isValid()) {
+            $author = $book->getAuthor();
             $author->setNbBooks($author->getNbBooks() + 1);
-            $book->setAuthor($author);
 
-            
-            $entitymanager->persist($book);
-            $entitymanager->flush();
-            
+            $entityManager->persist($book);
+            $entityManager->flush();
+
             return $this->redirectToRoute('book_index');
         }
-        return $this->render('book/new.html.twig',[
-            'authors'=>$authorepo->findAll(),
+
+        return $this->render('book/new.html.twig', [
+            'form' => $form->createView(),
         ]);
     }
 
+    #[Route('/{id}/edit', name: 'book_edit', methods: ['GET', 'POST'])]
+    public function edit(Request $request, Book $book, EntityManagerInterface $entityManager): Response
+    {
+        $form = $this->createForm(BookType::class, $book);
+        $form->handleRequest($request);
 
-    #[Route('/{id}/edit',name:'book_edit',methods:['GET','POST'])]
-    public function edit(Request $request,Book $book,EntityManagerInterface $entitymanager):Response {
-        if($request->isMethod('POST')){
-            $book->setTitle($request->request->get('title'));
-            $publishedDate = $request->request->get('published_date');
-            if ($publishedDate) {
-                $book->setPublicationDate(new \DateTime($publishedDate));
-            } else {
-                throw new \Exception('Published date is required');
-            }
-            $book->setEnabled($request->request->get('enabled'));
-            $author = $entitymanager->getRepository(Author::class)->find($request->request->get('author'));
+        if ($form->isSubmitted() && $form->isValid()) {
+            $author = $book->getAuthor();
             $author->setNbBooks($author->getNbBooks() + 1);
-            $book->setAuthor($author);
-            
-            $entitymanager->flush();
-            
+
+            $entityManager->flush();
+
             return $this->redirectToRoute('book_index');
         }
-        return $this->render('book/edit.html.twig',[
-            'book'=>$book,
-            'authors'=>$entitymanager->getRepository(Author::class)->findAll(),
+
+        return $this->render('book/edit.html.twig', [
+            'form' => $form->createView(),
         ]);
     }
 
-    #[Route('/{id}',name:'book_delete',methods:['POST'])]
-    public function delete(Request $request,Book $book,EntityManagerInterface $entitymanager):Response {
-        if($this->isCsrfTokenValid('delete'.$book->getId(),$request->request->get('_token'))){
-
-
+    #[Route('/{id}', name: 'book_delete', methods: ['POST'])]
+    public function delete(Request $request, Book $book, EntityManagerInterface $entityManager): Response
+    {
+        if ($this->isCsrfTokenValid('delete' . $book->getId(), $request->request->get('_token'))) {
             $author = $book->getAuthor();
             $author->setNbBooks($author->getNbBooks() - 1);
-            
-         
 
-            $entitymanager->remove($book);
-            $entitymanager->flush();
+            $entityManager->remove($book);
+            $entityManager->flush();
         }
+
         return $this->redirectToRoute('book_index');
     }
 
-
-
-    #[Route('/{id}',name:'book_show',methods:['GET'])]
-    public function show(Book $book):Response {
-        return $this->render('book/show.html.twig',[
-            'book'=>$book,
+    #[Route('/{id}', name: 'book_show', methods: ['GET'])]
+    public function show(Book $book): Response
+    {
+        return $this->render('book/show.html.twig', [
+            'book' => $book,
         ]);
     }
 
